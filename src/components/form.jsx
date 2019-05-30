7883a8b4edd5dbb02c9ffb3d79c9f1a3cd513462
Tanmay Khandelwal <tanmay.khandelwal@afourtech.com>
2019-05-30 15:55:36 +0530

    NewPage

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Center.css";
 import TextBox from "./input.jsx";
+
 class Form extends Component {
   state = {
     imageUrl: "https://i.ibb.co/0FCBcn5/blank-profile-picture.png"
@@ -9,28 +10,53 @@ class Form extends Component {
   render() {
     return (
       <div>
-        <h6 style={{ color: "grey" }}> AFour New Joinee </h6>
+        <h6 style={{ opacity: "0.5" }}> AFour New Joinee </h6>
         <h4>AFour Welcome Form</h4>
         <div style={{ backgroundColor: "#ebeff5" }}>
-          <figure>
-            <img
-              src={this.state.imageUrl}
-              class="Center"
-              style={{ height: 150 }}
-            />
-            <figcaption
-              style={{
-                color: "blue",
-                textAlign: "center"
-              }}
-            >
-              Change/Upload Photo
-            </figcaption>
-          </figure>
+          <img
+            src={this.state.imageUrl}
+            class="Center"
+            style={{ height: 150 }}
+          />
+          <figcaption
+            style={{
+              color: "blue",
+              textAlign: "center"
+            }}
+          >
+            Change/Upload Photo
+          </figcaption>
         </div>
-        <div style={{ fontStyle: "bold" }}>
-          First Name
+        <div style={{ backgroundColor: "#ebeff5" }}>
           <TextBox />
+        </div>
+
+        <div style={{ backgroundColor: "#ebeff5" }}>
+          <button
+            style={{
+              borderColor: "red",
+              backgroundColor: "#fff",
+              borderRadius: "100px",
+              color: "red",
+              padding: "5px 50px",
+              marginLeft: "800px"
+            }}
+          >
+            Cancel
+          </button>
+
+          <button
+            style={{
+              backgroundColor: "#428fd6",
+              borderRadius: "100px",
+              color: "white",
+              padding: "5px 50px",
+              marginTop: "50px",
+              marginLeft: "15px"
+            }}
+          >
+            Save
+          </button>
         </div>
       </div>
     );
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,29 +1,39 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
 
 const styles = theme => ({
   container: {
-    display: "flex",
     flexWrap: "wrap"
   },
   textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit
+    marginLeft: theme.spacing(10),
+    marginRight: theme.spacing(10),
+    marginTop: theme.spacing(5),
+    width: "500px"
   },
   dense: {
     marginTop: 20
   },
   menu: {
-    width: 100
+    width: 50
   }
 });
 
 class TextBox extends Component {
   state = {
-    firstname: ""
+    firstname: "Robinx",
+    lastname: "Banks",
+    qualification: "M.E.(CSE)",
+    emailid: "robinx.banks@afourteach.com",
+    hobbies: "Swimming,Traveling,Fishing",
+    favouritemovie: "Avengers",
+    favouritefood: "South Indian Food",
+    favouritebook: "Interno-by Dan Brown",
+    favquote: "Without a goal,you can't score"
   };
 
   handleChange = name => event => {
@@ -38,11 +48,86 @@ class TextBox extends Component {
     return (
       <form className={classes.container} noValidate autoComplete="off">
         <TextField
-          id="outlined-name"
+          id="first-name"
+          label="FirstName"
           className={classes.textField}
-          value={this.state.name}
-          onChange={this.handleChange("name")}
+          value={this.state.firstname}
+          onChange={this.handleChange("firstname")}
           variant="outlined"
+          style={{ opacity: "0.5", background: "#fff" }}
+        />
+        <TextField
+          id="last-name"
+          label="LastName"
+          className={classes.textField}
+          value={this.state.lastname}
+          onChange={this.handleChange("lastname")}
+          variant="outlined"
+          style={{ opacity: "0.5", background: "#fff" }}
+        />
+        <br />
+        <TextField
+          id="qualification"
+          label="Qualification"
+          className={classes.textField}
+          value={this.state.qualification}
+          onChange={this.handleChange("Qualification")}
+          variant="outlined"
+          style={{ opacity: "0.5", background: "#fff" }}
+        />
+        <TextField
+          id="email"
+          label="Email Id"
+          className={classes.textField}
+          value={this.state.emailid}
+          onChange={this.handleChange("Emailid")}
+          variant="outlined"
+          style={{ opacity: "0.5", background: "#fff" }}
+        />{" "}
+        <TextField
+          id="hobbies"
+          label="Hobbies"
+          className={classes.textField}
+          value={this.state.hobbies}
+          onChange={this.handleChange("Hobbies")}
+          variant="outlined"
+          style={{ opacity: "0.5", background: "#fff" }}
+        />{" "}
+        <TextField
+          id="favouritemovie"
+          label="Favourite Movie"
+          className={classes.textField}
+          value={this.state.favouritemovie}
+          onChange={this.handleChange("Favourite Movie")}
+          variant="outlined"
+          style={{ opacity: "0.5", background: "#fff" }}
+        />{" "}
+        <TextField
+          id="favouritefood"
+          label="Favourite Food"
+          className={classes.textField}
+          value={this.state.favouritefood}
+          onChange={this.handleChange("Favourite Food")}
+          variant="outlined"
+          style={{ opacity: "0.5", background: "#fff" }}
+        />{" "}
+        <TextField
+          id="favouritebook"
+          label="Favourite Book"
+          className={classes.textField}
+          value={this.state.favouritebook}
+          onChange={this.handleChange("Favourite Book")}
+          variant="outlined"
+          style={{ opacity: "0.5", background: "#fff" }}
+        />{" "}
+        <TextField
+          id="favouritequote"
+          label="Favourite Quote"
+          className={classes.textField}
+          value={this.state.favquote}
+          onChange={this.handleChange("Favourite Quote")}
+          variant="outlined"
+          style={{ opacity: "0.5", backgroundColor: "#fff" }}
         />
       </form>
     );
